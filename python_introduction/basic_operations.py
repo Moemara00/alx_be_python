df90ffee97de9d06c8c4bdfb38125e2deee8d843
Mohammed Emara <moemara034@gmail.com>
2024-09-15 00:54:11 +0300

    Add a file of Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+number1 + number2 == 15
+number1 - number2 == 5
+number1 * number2 == 50 
+
+print("Addition of",number1,"and",number2,"is",15)
+print("Subtraction of",number1,"and",number2,"is",5)
+print("Multiplication of",number1,"and",number2,"is",50)
