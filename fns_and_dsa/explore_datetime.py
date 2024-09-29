5122f21565ceb3fd952d434f4a9d623d12f1e5c0
Mohammed Emara <moemara034@gmail.com>
2024-09-29 20:05:24 +0300

    Explore datetime Module3

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -12,9 +12,9 @@ display_current_datetime()
 
 days_added = int(input("Enter a number of days: "))
 
-def alculate_future_date():
+def calculate_future_date():
     future_date = current_date +timedelta(days=days_added)
     
     print(future_date)
 
-alculate_future_date()
+calculate_future_date()
