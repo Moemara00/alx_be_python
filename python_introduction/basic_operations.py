a8acd8b52855bac1800ee5460177bec252ea9150
Moemara00 <moemara034@gmail.com>
2024-09-30 23:33:23 +0300

    basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -8,4 +8,3 @@ number1 * number2 == 50
 print("Addition of",number1,"and",number2,"is",15)
 print("Subtraction of",number1,"and",number2,"is",5)
 print("Multiplication of",number1,"and",number2,"is",50)
-print("Division of",number1,"and",number2,"is",2)
