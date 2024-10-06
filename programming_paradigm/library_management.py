6f71b73ea952bd42294ae068d8c0fca1bcfca5c6
Mohammed Emara <moemara034@gmail.com>
2024-10-06 23:11:21 +0300

    Implementing Basic OOP for a Library Management System

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,32 +1,37 @@
 class Book:
+    def __init__(self,title,author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
 
-    def __init__(self,title,author,_is_checked_out):
+    def check_out(self):
+        self._is_checked_out = True
 
-            self.title = title
-            self.author = author
-            self._is_checked_out = _is_checked_out
+    def return_book(self):
+        self._is_checked_out = False
 
-    def check_out_book(self,title):
-         
-        pass        
-    def return_book(title):
-         pass
-
-
+    def is_available(self):
+        return not self._is_checked_out
 
 class Library:
-      
-      def __init__(self) :
-            
-        self._books
+    def __init__(self):
+        self._books = []
 
+    def add_book(self,book):
+        self._books.append(book)
 
-      def add_book(self):
-           pass
-      def check_out_book(title):
-           pass
-      def return_book(title):
-           pass
-      def list_available_books(self):
-           pass
-      
+    def return_book(self,title):
+        for book in self._books:
+            if book.title == title:
+                self._books.remove(book)
+
+    def check_out_book(self,title):
+        for book in self._books:
+            if book.title == title:
+                book._is_checked_out = True
+                return f"{title} is found"
+            else:
+                return f"{title} is not found"
+    def list_available_books(self):
+        for book in self._books:
+             print(f"{book.title} by {book.author}")
