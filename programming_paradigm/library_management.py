9dc3b8118fdd51d6e9f7f44f5b00e36d421a9392
Mohammed Emara <moemara034@gmail.com>
2024-10-06 22:58:51 +0300

    second

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,32 @@
+class Book:
+
+    def __init__(self,title,author,_is_checked_out):
+
+            self.title = title
+            self.author = author
+            self._is_checked_out = _is_checked_out
+
+    def check_out_book(self,title):
+         
+        pass        
+    def return_book(title):
+         pass
+
+
+
+class Library:
+      
+      def __init__(self) :
+            
+        self._books
+
+
+      def add_book(self):
+           pass
+      def check_out_book(title):
+           pass
+      def return_book(title):
+           pass
+      def list_available_books(self):
+           pass
+      
