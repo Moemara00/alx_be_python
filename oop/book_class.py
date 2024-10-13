0c041f47dbc5a23308ad24f459e7eca775f80806
Mohammed Emara <moemara034@gmail.com>
2024-10-13 16:33:35 +0300

    Dive into Python Magic Methods2

--- oop/book_class.py
+++ oop/book_class.py
@@ -9,12 +9,12 @@ class Book:
         self.year = year
 
     # define a repr formate (more official)
-    def __repr__(self) :
+    def __repr__(self):
         
         return f"Book('{self.title}', '{self.author}', {self.year})"
 
     # defien a string message to show info about the object
-    def __str__(self) :
+    def __str__(self):
         
         return f"{self.title} by {self.author}, published in {self.year}"
 
