1ed55f425d0ad8ca968843f53ebb49af2478a27a
Mohammed Emara <moemara034@gmail.com>
2024-10-13 16:31:17 +0300

    Dive into Python Magic Methods

--- oop/book_class.py
+++ oop/book_class.py
@@ -7,17 +7,16 @@ class Book:
         self.title = title
         self.author = author
         self.year = year
-    
-    
-    # defien a string message to show info about the object
-    def __str__(self) :
-        
-        return f"{self.title} by {self.author}, published in {self.year}"
-    
+
     # define a repr formate (more official)
     def __repr__(self) :
         
         return f"Book('{self.title}', '{self.author}', {self.year})"
+
+    # defien a string message to show info about the object
+    def __str__(self) :
+        
+        return f"{self.title} by {self.author}, published in {self.year}"
 
     # define the message before deleting the object directly
     def __del__(self):
